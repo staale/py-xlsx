ac5befe732706084e43226083e6d5c9366f59eff
Ståle Undheim <staale.undheim@ums.no>
2013-12-17 08:40:48 +0100

    Updated name to deploy to pypi.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     version="0.2",
-    name='python-xlsx',
+    name='py-xlsx',
     description="""Tiny python code for parsing data from Microsoft's Office
     Open XML Spreadsheet format""",
     long_description="",
@@ -15,7 +15,7 @@ setup(
         'Programming Language :: Python',
     ],
     author='Staale Undheim',
-    author_email='github@staale.org',
+    author_email='staale@staale.org',
     url='http://github.com/staale/python-xlsx',
     packages=[
         "xlsx"
